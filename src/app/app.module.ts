f80184f9bb5d96cccbf08658f5ddaccf94789e25
Trainee <Trainee@KGFSL.COM>
2018-05-23 18:53:07 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+// import { MaterialModule } from './material.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
+
+  
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+    // MaterialModule 
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
 
 if (environment.production) {
   enableProdMode();
